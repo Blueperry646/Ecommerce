21f4d901b0d98e359784e0b7dbe3208358624c53
Blueperry646 <CG3037461@car.ifsp>
2026-03-10 07:32:44 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Topo from './components/topo'
+
+function App() {
+  return (
+    <>
+      <div>
+      <header>
+        <Topo></Topo>
+      </header>
+      </div>
+    </>
+  )
+}
+
+export default App
